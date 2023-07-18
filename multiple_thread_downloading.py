4ec283af764930aeab0aebb0f8e81db468a462c9
nyraa <112930946+nyraa@users.noreply.github.com>
2023-07-18 21:39:03 +0800

    add retry

--- multiple_thread_downloading.py
+++ multiple_thread_downloading.py
@@ -13,8 +13,8 @@ class mtd:
         self._store_base = store_base
         self._threading = 0
         self._pop = 0
-        self._finished = 0
         self.max_thread = max_thread
+        self.max_retry = 10
 
     def _worker(self):
         while self._download_queue.qsize() > 0:
@@ -26,17 +26,22 @@ class mtd:
         self._worker_running = False
 
     def _download(self, name):
-        try:
+        for retry in range(self.max_retry):
             print(f'Downloading {name}...({self._pop}/{self._length})\n', end='')
-            res = requests.get(self._base + name, headers=self._header)
-            res.raise_for_status()
-        except Exception as ex:
-            print(f'Error: {name}: {ex}')
+            try:
+                res = requests.get(self._base + name, headers=self._header)
+                res.raise_for_status()
+                break
+            except Exception as ex:
+                print(f'Error: {name}: {ex}')
+                print(f'Retrying...({retry + 1}/{self.max_retry})\n', end='')
+                continue
+        if retry == self.max_retry - 1:
+            print(f'Failed to download {name}')
             return
         with open(os.path.join(self._store_base, name), 'wb') as file:
             file.write(res.content)
         self._threading -= 1
-        self._finished += 1
 
     def push(self, name):
         self._download_queue.put(name)
