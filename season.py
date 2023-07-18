63589eb86e06ecb879452915cb784d2b3daf3820
nyraa <112930946+nyraa@users.noreply.github.com>
2023-07-19 00:48:22 +0800

    update season select resolution logic

--- season.py
+++ season.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     if len(sys.argv) > 2:
         resolution = int(sys.argv[2])
     else:
-        resolution = None
+        resolution = input('resolution index (-1=highest): ')
     metadata = acgDetail(s=s, parse_metadata=True)
     for ep, sn in metadata.sns.items():
         download_sn(sn, resolution=resolution if resolution is not None else None, group_dir_name=metadata.name, ep_dir_name=f"{ep}_{{sn}}_{{resolution}}", method='mtd')
