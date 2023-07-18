754e497c6d8a0096d9aa06686f308329f631d66d
nyraa <112930946+nyraa@users.noreply.github.com>
2023-07-19 00:27:51 +0800

    add download season entry

---
+++ season.py
@@ -0,0 +1,18 @@
+import sys
+from animefun import download_sn
+from acgDetail import acgDetail
+
+if __name__ == '__main__':
+    # read sn from argv or stdin
+    if len(sys.argv) > 1:
+        s = sys.argv[1]
+        print('s: %s' % s)
+    else:
+        s = input('sn: ')
+    if len(sys.argv) > 2:
+        resolution = int(sys.argv[2])
+    else:
+        resolution = None
+    metadata = acgDetail(s=s, parse_metadata=True)
+    for ep, sn in metadata.sns.items():
+        download_sn(sn, resolution=resolution if resolution is not None else None, group_dir_name=metadata.name, ep_dir_name=f"{ep}_{{sn}}_{{resolution}}", method='mtd')
