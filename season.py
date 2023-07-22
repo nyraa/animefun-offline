3fe5b084346d6e9e5595a76745aa241f7dce589a
nyraa <112930946+nyraa@users.noreply.github.com>
2023-07-23 00:55:48 +0800

    update prompt message

--- season.py
+++ season.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     if len(sys.argv) > 2:
         resolution = int(sys.argv[2])
     else:
-        resolution = input('resolution index (-1=highest): ')
+        resolution = int(input('resolution index: '))
     metadata = acgDetail(s=s, parse_metadata=True)
     for ep, sn in metadata.sns.items():
         download_sn(sn, resolution=resolution if resolution is not None else None, group_dir_name=metadata.name, ep_dir_name=f"{ep}_{{sn}}_{{resolution}}", method='mtd')
